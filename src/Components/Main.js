fc61f526ffdda4be02ca03104c539635f43f3932
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-18 18:16:54 +0200

    added main.js to app.js

---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './main.css';
+
+
+export default function main() {
+  return (
+    <div>
+      <div class="container">
+      <div class="col-2"><p>test1</p></div>
+      <div class="col-8"><p>test1</p></div>
+      <div class="col-2"><p>test1</p></div>
+      </div>
+    </div>
+  )
+}
