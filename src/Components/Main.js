bf600e80ea0f29ded86c681735ea34cfb527d4d1
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-18 18:32:11 +0200

    added main.js basic structure and css

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,9 +6,21 @@ export default function main() {
   return (
     <div>
       <div class="container">
-      <div class="col-2"><p>test1</p></div>
-      <div class="col-8"><p>test1</p></div>
-      <div class="col-2"><p>test1</p></div>
+        <div class="col-2"><p>test1</p></div>
+          <div class="col-8">
+            {/* TODOS LIST */}
+            {/* TITLE */}
+            <h1>JUST DO IT</h1>
+            {/* INPUT */}
+            <div></div>
+            {/* UL-TASKS */}
+            <div></div>
+            {/* PROGRESSBAR */}
+            <div></div>
+          </div>
+        <div class="col-2"><p>test1</p></div>
+      
+      
       </div>
     </div>
   )
