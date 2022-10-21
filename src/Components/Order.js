0780bfcf15900016538d9fc38150ddd29be84d59
Tiefengnom <111353598+Tiefengnom@users.noreply.github.com>
2022-10-21 13:52:52 +0200

    k

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {createElement, useState} from "react";
 import './order.css';
 
 
@@ -7,12 +7,27 @@ import './order.css';
 
 
 export default function Order() { 
-  
+  const [inputo, setinputo] = useState("")
+let d = document.querySelector("#inputlist")
+let ul = document.querySelector("#ul")
 
-  
+  function olli() {
+setinputo(d.value)
+console.log(inputo)
+  }
+
+  function handleClick(e) {
+    
+let li = document.createElement("li")
+li.innerText = inputo
+ul.appendChild(li)
+setinputo("")
+d.value = ""
+  }
+
   return <div id="todolist">
-  <input id="inputlist" placeholder="todo" />
-<ul> 
+  <input onChange={olli} id="inputlist" placeholder="todo" /><button onClick={handleClick} type="submit" >Click me</button>
+<ul id="ul"> 
     <li>testliste1</li>
     <li>testliste2</li>
     <li>testliste3</li>
