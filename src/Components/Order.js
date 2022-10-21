c57eca78224555a47ed5419c3ecfbeddd27415c7
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-21 17:19:54 +0200

    lost of css

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,4 +1,4 @@
-import React, { createElement, useState } from "react";
+import React, { useState } from "react";
 import './order.css';
 
 
@@ -7,29 +7,22 @@ import './order.css';
 
 
 export default function Order() {
-  let l = localStorage.getItem("todo")
-  console.log(l)
 
-  if (0 === 1) {
-    for (let i = 0; i < l.length; i++) {
-
-
-    }
-  }
   const [inputo, setinputo] = useState(" ")
-  let d = document.querySelector("#inputlist")
-  let ul = document.querySelector("#ul")
+  const d = document.querySelector("#inputlist")
+  const ul = document.querySelector("#ul")
 
   function olli() {
-    setinputo(d.value)
-    console.log(inputo)
+    const inputValue =document.querySelector("#inputlist").value
+    console.log(inputValue)
+    const input = inputValue
+    setinputo(input)
   }
-const storedTasks = []
   function handleClick(e) {
     if(inputo === ""){
       alert("You need a Task")
     }else{
-      let li = document.createElement("li")
+      const li = document.createElement("li")
       const task = document.createElement("input")
       const checkbox = document.createElement('input')
       const edit = document.createElement("button")
@@ -41,6 +34,7 @@ const storedTasks = []
       task.value = inputo
       task.disabled = true
       task.className = "textarea"
+      ul.className = "list-unstyled"
 
       // creates the edit button 
       edit.className = "edit"
@@ -61,14 +55,11 @@ const storedTasks = []
   return <div id="todolist">
     <div className="taskBar">
       <input onChange={olli} id="inputlist" placeholder="todo" />
-      <button onClick={handleClick} type="submit" >Click me</button>
+      <button 
+      onClick={handleClick} 
+      type="submit" >Click me</button>
     </div>
     <ul id="ul">
-      <li>testliste1</li>
-      <li>testliste2</li>
-      <li>testliste3</li>
-      <li>testliste4</li>
-
     </ul>
   </div>
 }
