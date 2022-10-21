15a090eb5f4dd183b470dd56aca9227889ce2b4b
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-21 15:03:27 +0200

    formating

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,4 +1,4 @@
-import React, {createElement, useState} from "react";
+import React, { createElement, useState } from "react";
 import './order.css';
 
 
@@ -6,44 +6,48 @@ import './order.css';
 
 
 
-export default function Order() { 
- let l = localStorage.getItem("todo")
- console.log(l)
- 
-  if ( 0 ===1) {
-  for (let i = 0 ; i<l.length;i++) {
+export default function Order() {
+  let l = localStorage.getItem("todo")
+  console.log(l)
+
+  if (0 === 1) {
+    for (let i = 0; i < l.length; i++) {
 
 
-  }}
+    }
+  }
   const [inputo, setinputo] = useState("")
-let d = document.querySelector("#inputlist")
-let ul = document.querySelector("#ul")
+  let d = document.querySelector("#inputlist")
+  let ul = document.querySelector("#ul")
 
   function olli() {
-setinputo(d.value)
-console.log(inputo)
+    console.log(d.value)
+    console.log(inputo)
   }
 
   function handleClick(e) {
-    
-let li = document.createElement("li")
-li.innerText = inputo
-ul.appendChild(li)
-let k = JSON.stringify(inputo)
-localStorage.setItem("todo" , k)
-setinputo("")
-d.value = ""
-
+    if(d.value === ""){
+      alert("You need a Task")
+    }else{
+      let li = document.createElement("li")
+      li.innerText = inputo
+      ul.appendChild(li)
+      let k = JSON.stringify(inputo)
+      localStorage.setItem("todo", k)
+      setinputo("")
+      d.value = ""
+      setinputo("")
+    }
   }
 
   return <div id="todolist">
-  <input onChange={olli} id="inputlist" placeholder="todo" /><button onClick={handleClick} type="submit" >Click me</button>
-<ul id="ul"> 
-    <li>testliste1</li>
-    <li>testliste2</li>
-    <li>testliste3</li>
-    <li>testliste4</li>
-  
-  </ul>
+    <input onChange={olli} id="inputlist" placeholder="todo" /><button onClick={handleClick} type="submit" >Click me</button>
+    <ul id="ul">
+      <li>testliste1</li>
+      <li>testliste2</li>
+      <li>testliste3</li>
+      <li>testliste4</li>
+
+    </ul>
   </div>
-  }
+}
